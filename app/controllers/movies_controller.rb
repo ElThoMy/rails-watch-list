06e29d5c456533522fc2efb8260b377d5e1f1dd1
Thomas <t.vareilhes@gmail.com>
2021-04-29 22:24:00 +0200

    add movie to a list is working. Missing review feature

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,43 @@
 class MoviesController < ApplicationController
+  before_action :find_movie, only: [:show, :create, :edit, :update, :destroy]
+
+  def index
+    @movies = Movie.all
+  end
+
+  def show
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(review_params)
+    @movie.save
+    redirect_to movies_path
+  end
+
+  def edit
+  end
+
+  def update
+    @movie = @movie.update(review_params)
+    @movie.save
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie.destroy
+    redirect_to movies_path
+  end
+
+  private
+  def find_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def review_params
+    params.require(:movie).permit([:title, :overview, :rating])
+  end
 end
